75b2b69325dd08e05414d7eafad90aba9d1dff08
Sharon <Sharonaquilla28@gmail.com>
2024-04-04 15:31:45 +0300

    adds event lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+ function moveDodgerLeft(){
+    const left = parseInt(dodger.style.left)
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`
+    }
+ }
+
+ function moveDodgerRight(){
+    const left = parseInt(dodger.style.left)
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+ }
+
+ document.addEventListener("keydown", event => {
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft()
+    } else if(event.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+ })
